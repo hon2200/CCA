0576eba16b4c5525c1de49f647cc82f358567a39
renan <1797618933@qq.com>
2025-04-19 13:04:21 +0800

    Coders add

--- Src/Clap/Assets/Scripts/Game Logic Systems/Game Variables/People.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class People : MonoBehaviour
-{
-    public int initialPeople = 4;
-    public int alivePeople = 4;
-}
