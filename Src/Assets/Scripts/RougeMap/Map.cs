7c87ef9607e4d0bdf558269c1f1d5e977af7b453
hon2200 <1278042070@qq.com>
2026-01-29 19:46:01 +0800

    map things

---
+++ Src/Assets/Scripts/RougeMap/Map.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//产生Map
+public class Map
+{
+    public long seed;//种子
+    public List<MapNode> Nodes { get; set; }
+
+}
